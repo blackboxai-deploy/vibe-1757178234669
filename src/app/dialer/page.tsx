f8d108a562b7d4e7584b4f34ee46f4f8092d2e09
blackboxai-deploy <deploy@blackboxai.dev>
2025-09-06 22:34:03 +0530

    Add src/app/dialer/page.tsx

---
+++ src/app/dialer/page.tsx
@@ -0,0 +1,12 @@
+'use client';
+
+import { Layout } from '@/components/layout/Layout';
+import { DialerInterface } from '@/components/dialer/DialerInterface';
+
+export default function DialerPage() {
+  return (
+    <Layout>
+      <DialerInterface />
+    </Layout>
+  );
+}
